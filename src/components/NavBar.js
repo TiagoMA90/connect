32bd02b531dfd80ea2a8ebb1a6f6a5885e86a9ac
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-20 10:21:14 +0000

    handle routing for home, signin & signup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,28 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+        <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        </Navbar.Brand></NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
                 <i class="fa-solid fa-people-roof"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
                 <i class="fa-solid fa-person-walking"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
                 <i class="fa-solid fa-person-walking-dashed-line-arrow-right"></i>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
