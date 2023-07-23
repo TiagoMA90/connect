bf30ff131a789430cd4861855d5cb133c03481f5
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-23 22:33:34 +0000

    apply access and refresh tokens to CurrentUserContext

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://djangorestframework-api-38c4a098777a.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
