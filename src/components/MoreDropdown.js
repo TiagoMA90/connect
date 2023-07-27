e81fc60dd4cc3cf53f926410fde209cdb42758dd
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-27 20:00:23 +0000

    create dropdown options to edit or delete user posts

---
+++ src/components/MoreDropdown.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../styles/MoreDropdown.module.css";
+
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+    <i
+    className ="fa-solid fa-caret-down"
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    />
+  ));
+
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
+  return (
+    <Dropdown className="ml-auto">
+      <Dropdown.Toggle as={ThreeDots} />
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleEdit}
+          aria-label="edit"
+        >
+          <i className="fa-solid fa-pencil" />
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleDelete}
+          aria-label="delete"
+        >
+          <i className="fa-solid fa-trash" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
