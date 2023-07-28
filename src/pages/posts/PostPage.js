c88c10a35dfe0af9da953c862a1b66934558cfe8
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-28 21:10:45 +0000

    apply infiniteScroll and prps to the comments in PostPage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,13 +5,20 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import { fetchMoreData } from "../../utils/utils";
+
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import Post from "./Post";
-import Comment from '../comments/Comment';
 
+import Comment from '../comments/Comment';
 import CommentCreateForm from "../comments/CommentCreateForm";
+
+import Asset from "../../components/Asset";
+
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
@@ -56,13 +63,24 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map(comment => (
-              <Comment key={comment.id} {...comment} setPost={setPost} setComments={setComments} />
-            ))
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
-            <span>There are no comments. Be the first one to comment!</span>
+            <span>No comments, be the first one to submit!</span>
           ) : (
-            <span>No comments...</span>
+            <span>No comments... This section is empty.</span>
           )}
         </Container>
       </Col>
