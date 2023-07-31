f166bea07f156110ee25c4f50924c002e917e1f1
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-31 08:33:35 +0000

    create Proile Header for ProfilePage.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostEditForm from "./pages/posts/PostEditForm";
 import PostsPage from "./pages/posts/PostsPage";
@@ -25,6 +26,7 @@ function App() {
           <Route exact path="/liked" render={() => < PostsPage message="We could not find what you were looking for. Search for another keyword or like a post" filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} />} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
