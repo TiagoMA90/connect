9e1d066ae16b0d5ae9c783ce9ed63d3aab263e7b
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-31 10:41:26 +0000

    resort source code and follow the tutorial again to find errors

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -16,9 +16,8 @@ import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 import PopularProfiles from "./PopularProfiles";
-
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import { useProfileData } from "../../contexts/ProfileDataContext";
@@ -33,42 +32,49 @@ function ProfilePage() {
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
 
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const [{data: pageProfile}] = await Promise.all([
-        axiosReq.get(`/profiles/${id}`)
+          axiosReq.get(`/profiles/&{id}/`)
         ])
-        setProfileData(prevState => ({ ...prevState, pageProfile: {results: [pageProfile]}
+        setProfileData(prevState => ({
+          ...prevState,
+          pageProfile: {results: [pageProfile]}
         }))
         setHasLoaded(true);
       } catch(err) {
-        console.log(err)
+        console.log(err);
       }
-    }
-     fetchData() 
-  }, [id, setProfileData])
+    };
+    fetchData();
+  }, [id, setProfileData]);
 
   const mainProfile = (
     <>
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
-          <Image className={styles.ProfileImage} roundedCircle src={profile?.image} />
+          <Image
+            className={styles.ProfileImage}
+            roundedCircle
+            src={profile?.image}
+          />
         </Col>
         <Col lg={6}>
-        <h3 className="m-2">{profile?.owner}</h3>
+          <h3 className="m-2">{profile?.owner}</h3>
           <Row className="justify-content-center no-gutters">
             <Col xs={3} className="my-2">
               <div>{profile?.posts_count}</div>
-              <div>Posts</div>
+              <div>posts</div>
             </Col>
             <Col xs={3} className="my-2">
               <div>{profile?.followers_count}</div>
-              <div>Followers</div>
+              <div>followers</div>
             </Col>
             <Col xs={3} className="my-2">
               <div>{profile?.following_count}</div>
-              <div>Following</div>
+              <div>following</div>
             </Col>
           </Row>
         </Col>
@@ -80,18 +86,18 @@ function ProfilePage() {
                 className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
                 onClick={() => {}}
               >
-                unfollow
+                Unfollow
               </Button>
             ) : (
               <Button
                 className={`${btnStyles.Button} ${btnStyles.Black}`}
                 onClick={() => {}}
               >
-                follow
+                Follow
               </Button>
             ))}
         </Col>
-        { profile?.content && (<Col className="p-3">{profile.content}</Col>)}
+        {profile?.content && <Col className="p-3">{profile.content}</Col>}
       </Row>
     </>
   );
