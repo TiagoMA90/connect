35ac4020bd3548ad0117d3d09b5978c9067449ab
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-09 08:50:55 +0000

    fix profile_id property in apps.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import NotFound from './components/NotFound';
 
 function App() {
   const currentUser = useCurrentUser();
-  const profile_id = currentUser?.profile || "";
+  const profile_id = currentUser?.profile_id || "";
 
   return (
     <div className={styles.App}>
