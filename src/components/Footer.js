9848af44d2367c97cc91f2bf4178c463a6c40725
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-19 19:25:24 +0000

    conclude Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
   return (
-    <Container className={`${styles.container} p-4 mt-2`} style={{ border: '1px solid #ccc', backgroundColor: 'white' }}>
+    <Container className={`${styles.container} p-4 mt-2`}>
       <div className={styles.contactContainer}>
-        <div className={styles.contactAndIconsContainer} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <span style={{ display: 'inline-block', marginRight: '10px' }}>Contact us:</span>
-          <div className={styles.iconContainer} style={{ display: 'inline-block' }}>
+        <div className={styles.contactAndIconsContainer}>
+          <span>Contact us: </span>
+          <div className={styles.iconContainer}>
             <NavLink
               className={styles.NavLink}
               activeClassName={styles.Active}
@@ -32,9 +32,12 @@ const Footer = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <i className="fab fa-twitter"></i>
+              <i class="fa-brands fa-x-twitter"></i>
             </a>
           </div>
+        </div>
+        <div className={styles.copyright}>
+          <span>© 2023 Copyright • Connect</span>
         </div>
       </div>
     </Container>
