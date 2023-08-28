0478caeacf74b778ca20e9989b746f57b19a8a72
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-28 19:37:22 +0000

    remake and simplify Report.js

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,72 +1,58 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import axios from 'axios';
+import { axiosRes } from "../api/axiosDefaults";
+import { Button, Modal, Form } from "react-bootstrap";
 
-function Report({ post }) {
+const Report = ({ post }) => {
   const [showModal, setShowModal] = useState(false);
   const [reason, setReason] = useState("");
-  const [isReporting, setIsReporting] = useState(false);
-  const [error, setError] = useState("");
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
 
-  const handleReport = async () => {
-    setIsReporting(true);
-    setError("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      await axios.post(`/reports/`, { post: post, reason: reason }); // Tutor!Help! - This Endpoint does not communicate with the API???!
-      setShowModal(false);
-      // Fixthis: Create a component here to redirect users to a a success page!
-    } catch (error) {
-      console.error("Error reporting post:", error);
-      setError("An error occurred while reporting the post. Please try again later.");
-    } finally {
-      setIsReporting(false);
+      await axiosRes.post(`/reports/`, { post: post.id, reason });
+      handleClose();
+      // Create a Component/Success message if the Report succeeds.
+    } catch (err) {
+      console.log(err);
+      // Handle the error, show an error message, etc.
     }
   };
 
   return (
     <>
-      <Button variant="link" onClick={() => setShowModal(true)}>
-        <i className="fa-regular fa-flag" />
-      </Button>
-
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
+      <span onClick={handleShow}>
+        <i className="fas fa-flag" />
+      </span>
+      <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Report Post</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {error && <p className="error-message">{error}</p>}
-          <p>Choose a reason for reporting this post:</p>
-          <select
-            value={reason}
-            onChange={(e) => setReason(e.target.value)}
-            disabled={isReporting}
-          >
-            <option value="">Select Reason</option>
-            <option value="spam_flame">Spamming and Flaming</option>
-            <option value="inappropriate">Inappropriate Content</option>
-            <option value="other">Other</option>
-          </select>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="reportReason">
+              <Form.Label>Reason for Reporting</Form.Label>
+              <Form.Control
+                as="select"
+                value={reason}
+                onChange={(e) => setReason(e.target.value)}
+                required
+              >
+                <option value="">Select a reason...</option>
+                <option value="spam_flame">Spamming and Flaming</option>
+                <option value="inappropriate">Inappropriate Content</option>
+                <option value="other">Other</option>
+              </Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Report
+            </Button>
+          </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={() => setShowModal(false)}
-            disabled={isReporting}
-          >
-            Cancel
-          </Button>
-          <Button
-            variant="primary"
-            onClick={handleReport}
-            disabled={!reason || isReporting}
-          >
-            {isReporting ? "Reporting..." : "Report"}
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
-}
+};
 
 export default Report;
